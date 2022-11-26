9091ea5d032c4237278fa1f422fd56efb74dd8c0
Leonid <walya7534@yandex.ru>
2022-11-26 21:27:52 +0700

    added Hamster Bob

--- Praktikum.java
+++ Praktikum.java
@@ -1,7 +1,11 @@
 public class Praktikum {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Hamster bob = new Hamster();
+        System.out.println("Hello "+ bob.name);
     }
 }
-
+class Hamster{
+    String name = "Bob";
+}
         
